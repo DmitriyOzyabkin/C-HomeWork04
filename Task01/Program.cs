82bd03936ce1546ce449c752ef00b9a6b0ff18c4
Dmitriy Ozyabkin <ozyabkin.d@gmail.com>
2024-03-19 19:13:39 +0300

    Task01 completed

---
+++ Task01/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли.
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+
+
+bool IsNotValidEnter(string number)
+{
+    if (number == "q") return false;
+    int checkSum = SumDigits(number);
+    if (checkSum % 2 == 0) return false;
+    return true;
+}
+
+int SumDigits(string number)
+{
+    int num = Convert.ToInt32(number);
+    int sum = 0;
+    while (true)
+    {
+        int digit = num % 10;
+        sum += digit;
+        num /= 10;
+        if (num == 0)
+        return sum;
+        
+    }
+    Console.WriteLine(sum);
+    return sum;
+}
+
+while (true)
+{
+    Console.Write("Enter number: ");
+    string enteredNumber = Console.ReadLine();
+    if (IsNotValidEnter(enteredNumber) == false) break;
+}
