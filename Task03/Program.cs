04ff1fdefc37fff2a05907757aa9f8b925012502
Dmitriy Ozyabkin <ozyabkin.d@gmail.com>
2024-03-20 05:13:50 +0300

    Task03 began

---
+++ Task03/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+int[] CreateArrayRndNum(int size, int min, int max)
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write('[');
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+
+        else
+        {
+            {
+                Console.Write(array[i]);
+            }
+        }
+    }
+    Console.Write(']');
+}
+
+int[] ReverseArray(int[] array)
+{
+    
+}
+int[] initArray = CreateArrayRndNum(6, 1, 10);
+
+PrintArray(initArray);
+Console.Write($" => [reversed array]");
+
